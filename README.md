218e670c61c40fa0bebd9ba1f0bb7fb6c29df57b
Guido Müller <guido.mueller@outloook.de>
2024-04-10 00:09:12 +0200

    Add jar-with-dependencies

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Mvn Test Project
 
-    mvn archetype:generate "-DgroupId=com.mycompany.foo" "-DartifactId=foolib"  "-DarchetypeArtifactId=maven-archetype-quickstart" "-DarchetypeVersion=1.4" "-DinteractiveMode=false"s
+    mvn archetype:generate "-DgroupId=com.mycompany.foo" "-DartifactId=foolib"  "-DarchetypeArtifactId=maven-archetype-quickstart" "-DarchetypeVersion=1.4" "-DinteractiveMode=false"
+
+## Build
+
+    mvn clean package assembly:single
+
+## Run
+
+    java --add-opens java.base/java.lang=ALL-UNNAMED -jar .\target\foolib-1.0-SNAPSHOT-jar-with-dependencies.jar
--- src/main/java/com/mycompany/foo/App.java
+++ src/main/java/com/mycompany/foo/App.java
@@ -1,16 +1,27 @@
 package com.mycompany.foo;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
 
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        Injector injector = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                // Bindings go here
+                bind(Greeting.class).to(GreetingImpl.class);
+            }
+        });
+
+        // Use the injector to get an instance of a class
+        Greeting myClass = injector.getInstance(Greeting.class);
+        System.out.println(myClass.sayHello());
     }
-
 
 }
